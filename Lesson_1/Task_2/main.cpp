7d86bc914b3f8748e49d1447021459bffc255419
Ast1o <dimam4659@gmail.com>
2025-05-17 14:57:28 +0300

    fixed the copy-paste

--- Lesson_1/Task_2/main.cpp
+++ Lesson_1/Task_2/main.cpp
@@ -21,29 +21,25 @@ int main() {
         switch (operation) {
             case '+':
                 result = first_number + second_number;
-                cout << "Result: " << result << endl;
+                cout << first_number << " + " << second_number << " = " << result << endl;
                 break;
-
             case '-':
                 result = first_number - second_number;
-                cout << "Result: " << result << endl;
+                cout << first_number << " - " << second_number << " = " << result << endl;
                 break;
-
             case '*':
                 result = first_number * second_number;
-                cout << "Result: " << result << endl;
+                cout << first_number << " * " << second_number << " = " << result << endl;
                 break;
-
             case '/':
                 if (second_number != 0) {
                     result = first_number / second_number;
-                    cout << "Result: " << result << endl;
+                    cout << first_number << " / " << second_number << " = " << result << endl;
                 } else {
                     cout << "Error: Division by zero." << endl;
                     return 1;
                 }
                 break;
-
             default:
                 cout << "Error: Invalid operation." << endl;
                 return 1;
@@ -51,16 +47,19 @@ int main() {
     } else if (operation == '^') {
         cout << "Enter the degree: ";
         cin >> degree;
-
+        if (degree < 0) {
+            cout << "\nError: Degree cannot be negative." << endl;
+            return 1;
+        }
         result = pow(first_number, degree);
-        cout << "\nFirst number raised to the degree of " << degree << ": " << result << endl;
+        cout << "\n" << first_number << " raised to the degree of " << degree << " = " << result << endl;
 
     } else if (operation == 'r') {
         if (first_number < 0) {
             cout << "\nError: Cannot take square root of the first number (negative)." << endl;
         } else {
             result = sqrt(first_number);
-            cout << "\nSquare root of the first number: " << result << endl;
+            cout <<  "\nSquare root of " << first_number << " = " << result << endl;
         }
     } else {
         cout << "Error: Invalid operation." << endl;
