11636210480d54468f7b9fdb25fc52b2b7db82f1
Ast1o <dimam4659@gmail.com>
2025-10-27 22:16:34 +0200

    Compleate task 2

---
+++ Lesson_1/Task_2/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    char operation;
+    double numberF = 0, numberL = 0;
+
+    cout << "Enter operation (+, -, *, /, ^, r): ";
+    cin >> operation;
+
+    if(operation == 'r'){
+        cout << "Enter one number: ";
+        cin >> numberF;
+        cout << "You entered: " << numberF << endl;
+    }else{
+        cout << "Enter two numbers: ";
+        cin >> numberF >> numberL;
+        cout << "You entered: " << numberF << " and " << numberL << endl;
+    }
+
+    switch (operation) {
+    case '+':
+        cout << numberF + numberL << endl;
+        break;
+    case '-':
+        cout << numberF - numberL << endl;
+        break;
+    case '*':
+        cout << numberF * numberL << endl;
+        break;
+    case '/':
+        if (numberL != 0)
+            cout << numberF / numberL << endl;
+        else
+            cout << "Error: Division by zero" << endl;
+        break;
+    case '^':
+        cout << pow(numberF, numberL) << endl;
+        break;
+    case 'r':
+        if (numberF >= 0) {
+            cout << "Square root of " << numberF << " = " << sqrt(numberF) << endl;
+        } else {
+            cout << "Error: Cannot compute square root of a negative number" << endl;
+        }
+        break;
+    default:
+        cout << "Error: Unknown operation" << endl;
+        break;
+    }
+}
